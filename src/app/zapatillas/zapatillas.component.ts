64dd21ec891a82346bf92e89f9ccdd98dc3e0783
Erick Torres <kcire_792_efta@hotmail.com>
2019-06-16 13:28:07 -0500

    Ultimos cambios fueron explicando las directivas ngClass en zapatillas

--- src/app/zapatillas/zapatillas.component.ts
+++ src/app/zapatillas/zapatillas.component.ts
@@ -9,8 +9,13 @@ import { Zapatilla } from '../models/zapatilla';
  export class ZapatillasComponent implements OnInit{
  	public titulo: string = "Componente de zapatilla";
  	public zapatillas: Array<Zapatilla>;
+ 	public mark: String[];
+ 	public color: string;
+ 	public miMarca: string;
 
  	constructor(){
+ 		this.color = "rojo";
+ 		this.mark = new Array();
  		this.zapatillas = [
  			new Zapatilla('Reebook Clasi', 'Reebook',10, 'Blanco', true),
  			new Zapatilla('Nike ryzen', 'Nike',50, 'Negro', true),
@@ -19,6 +24,41 @@ import { Zapatilla } from '../models/zapatilla';
  	}
 
  	ngOnInit(){
- 		console.log(this.zapatillas);
+ 		// console.log(this.zapatillas);
+
+ 		this.marcas();
+ 	}
+
+ 	marcas(){
+ 		this.zapatillas.forEach((value, index) => {
+ 			if (this.mark.indexOf(value.marca) < 0) {
+ 				this.mark.push(value.marca);
+ 			}
+ 		
+ 		});
+
+ 		console.log(this.mark);
+ 	}
+
+ 	getMarca(){
+ 		alert(this.miMarca);
+ 	}
+
+ 	addMarca(){
+ 		this.mark.push(this.miMarca);
+
+ 		this.miMarca = '';
+ 	}
+
+ 	borrarMarca(index) {
+ 		this.mark.splice(index,1);
+ 	}
+
+ 	onBlur(){
+ 		console.log("Has salido del input");
+ 	}
+
+ 	mostrarPalabra(){
+ 		alert(this.miMarca);
  	}
  }
